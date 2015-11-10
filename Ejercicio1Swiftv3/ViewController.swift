8265fbbeb4b78530aaae8cc01f91ea152a24ccb5
yolandamartre <yolandamartre@hotmail.com>
2015-11-09 23:00:08 -0600

    Ejercicio 1 del módulo 2

--- Ejercicio1Swiftv3/ViewController.swift
+++ Ejercicio1Swiftv3/ViewController.swift
@@ -13,6 +13,63 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let numeros = 1...100
+        
+        for num in numeros
+        {
+            if num % 2 == 0
+            {
+                if num >= 30 && num <= 40
+                {
+                    if num % 5 == 0
+                    {
+                        print ("# \(num) Par Bingo Viva Swift!!!")
+                    }
+                    else
+                    {
+                        print ("# \(num) Par Viva Swift!!!")
+                    }
+                }
+                else
+                {
+                    if num % 5 == 0
+                    {
+                        print ("# \(num) Par Bingo!!!   ")
+                    }
+                    else
+                    {
+                        print ("# \(num) Par!!!   ")
+                    }
+                }
+            }
+            if (num % 2 != 0)
+            {
+                if num >= 30 && num <= 40
+                {
+                    if num % 5 == 0
+                    {
+                        print ("# \(num) Impar Bingo Viva Swift!!!   ")
+                    }
+                    else
+                    {
+                        print ("# \(num) Impar Viva Swift!!!   ")
+                    }
+                }
+                else
+                {
+                    if num % 5 == 0
+                    {
+                        print ("# \(num) Impar Bingo!!!   ")
+                    }
+                    else
+                    {
+                        print ("# \(num) Impar!!!   ")
+                    }
+                }
+            }
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
